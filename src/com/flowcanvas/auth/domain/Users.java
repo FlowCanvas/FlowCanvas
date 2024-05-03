dd13b62828b331198437d741953fe7f074a15f7e
DESKTOP-4U4G4G2\KOSA <dmswjd4566@hanmail.net>
2024-05-03 14:50:17 +0900

    Feat: 로그인 기능, 회원가입 기능 구현    - 암호화 클래스 생성 (Encrypt.java)    - 회원가입 시 비밀번호 암호화로 저장되게 구현    - 로그인 시 암호화된 비밀번호로 체크

---
+++ src/com/flowcanvas/auth/domain/Users.java
@@ -0,0 +1,19 @@
+package com.flowcanvas.auth.domain;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Users {
+
+    private int userId;
+    private String email;
+    private String password;
+    private String nickName;
+    private LocalDateTime createAt;
+}
