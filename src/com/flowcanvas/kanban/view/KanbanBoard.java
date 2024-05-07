fb52cb81c2ca07152abb690fa77b0bd9fcd341f1
KOSA <KOSA@DESKTOP-4U4G4G2>
2024-05-07 11:17:49 +0900

    Change: 프로젝트 삭제 / 코드 수정

--- src/com/flowcanvas/kanban/view/KanbanBoard.java
+++ src/com/flowcanvas/kanban/view/KanbanBoard.java
@@ -308,10 +308,18 @@ public class KanbanBoard {
 	// 프로젝트 삭제
 	private void delProject(ProjectsDto projectsDto) {
 		
-		projectDao.delProject(projectsDto.getProjectId());
-		
-		// 프로젝트 리스트 조회
-		selProjects();
+	    int checkDelMessage = 
+	    		JOptionPane.showConfirmDialog(frame, "프로젝트를 삭제하시겠습니까?", "삭제 확인", 
+	    				JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+
+	    if (checkDelMessage == JOptionPane.YES_OPTION) {
+	    	
+	        projectDao.delProject(projectsDto.getProjectId());
+	        // 프로젝트 리스트 조회
+	        selProjects();
+	        
+	        JOptionPane.showMessageDialog(frame, "프로젝트가 삭제되었습니다.", "삭제 완료", JOptionPane.INFORMATION_MESSAGE);
+	    }
 	}
 	
 	
